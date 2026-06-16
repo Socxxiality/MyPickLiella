3093257bd5628871944566d54b4b683191ff93a5
Socxxiality <alihisyam@gmail.com>
2026-06-16 17:45:53 +0700

    Minor Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "@fontsource/manrope/600.css";
 import "@fontsource/manrope/700.css";
 import "@fontsource/manrope/800.css";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "My Pick Liella!",
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script defer src="https://analytics.jayjay.li/script.js" data-website-id="dfacb552-190a-472c-84e2-9d9476bbc6d4" />
     </html>
   );
 }
